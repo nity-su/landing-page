d1d402448233558e2579b00bb17c80c478b2e5d3
nity-su <7anlyn@gmail.com>
2022-11-09 00:33:43 +0900

    origin main conflict resolve

--- src/App.js
+++ src/App.js
@@ -4,46 +4,16 @@ import SplinePage from "@components/SplinePage";
 import BuyLinkPage from "@components/BuyLinkPage";
 import ContentsPage from "@components/ContentsPage";
 import QuestionAnswerPage from "@components/QuestionAnswerPage";
-
-<<<<<<< HEAD
-=======
-import GlobalStyle from "./GlobalStyle";
 import Footer from "./components/Footer";
-
-const FirstPage = styled.div`
-  width: 100%;
-  height: 100vh;
-  background-color: pink;
-`;
-const SecondPage = styled.div`
-  width: 100%;
-  height: 100vh;
-  background-color: rebeccapurple;
-`;
-
-const ThreePage = styled.div`
-  width: 100%;
-  height: 100vh;
-  background-color: yellowgreen;
-`;
->>>>>>> aca319f
 function App() {
   return (
     <>
       <GlobalStyle />
-<<<<<<< HEAD
       <SplinePage />
       <ContentsPage />
       <BuyLinkPage />
       <QuestionAnswerPage />
-=======
-      <div>
-        <FirstPage />
-        <SecondPage />
-        <ThreePage />
-        <Footer />
-      </div>
->>>>>>> aca319f
+      <Footer />
     </>
   );
 }
