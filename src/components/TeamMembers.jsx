4ad07104f86aee0629f6062423abe885976c95e9
jhnam2250 <rs225@naver.com>
2022-11-09 16:07:48 +0900

    바뀐거없음

--- src/components/TeamMembers.jsx
+++ src/components/TeamMembers.jsx
@@ -3,25 +3,41 @@ import styled from "styled-components";
 const Container = styled.div`
   width: 100%;
   height: 30vh;
-  background-color: #fcd111;
+  background-color: #ffe262;
   display: flex;
   justify-content: center;
+  align-items: center;
+`;
+
+const MemberProfileWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
 `;
 
 const MemberBox = styled.div`
-  width: 100px;
+  width: 142px;
+  height: 32px;
   background-color: white;
+  border-radius: 53px;
   display: flex;
   font-size: 10px;
-  color: black;
+  color: #2329d6;
   justify-content: center;
   align-items: center;
+`;
+
+const MemberIntroduction = styled.div`
+  height: 82px;
+  width: 222px;
+  left: 89px;
+  top: 0px;
 `;
 
 export default function TeamMembsrs() {
   return (
     <Container>
-      <MemberBox>Team Members</MemberBox>
+      <MemberBox>Team Members</MemberBox>,<MemberBox>Team Members</MemberBox>
     </Container>
   );
 }
