d487b6820230753be13358663a726a4dde949668
azerc <zizimoos@likelion.org>
2022-11-09 13:03:32 +0900

    :lipstick: footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,30 +1,61 @@
 import React from "react";
 import styled from "styled-components";
 
-const Container = styled.div`
-  width: 100%;
-  height: 600px;
+const FooterContainer = styled.div`
+  width: 100vw;
+  height: 300px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
   color: #fdfbfc;
   background-color: #1c1817;
 `;
 
+const LogoLion = styled.div`
+  width: 1024px;
+  display: flex;
+  flex-direction: column;
+  font-size: 14px;
+`;
+const TitleText = styled.div`
+  width: 1024px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 32px;
+`;
+const EndingCredit = styled.div`
+  width: 1024px;
+  margin-top: 20px;
+  div:nth-child(1) {
+    margin-bottom: 10px;
+    font-size: 12px;
+  }
+  div:nth-child(2) {
+    margin-bottom: 10px;
+    font-size: 14px;
+  }
+`;
+
 function Footer() {
   return (
-    <Container>
-      <div>
+    <FooterContainer>
+      <LogoLion>
         <div>LIKELION</div>
         <div>BLOCKCHAIN</div>
         <div>SCHOOL</div>
-      </div>
-      <div>
+      </LogoLion>
+      <TitleText>
         <div>TEAM 15</div>
         <div>PORTFOLIO</div>
-      </div>
-      <div>
+      </TitleText>
+      <EndingCredit>
         <div>도와주신분들</div>
-        <div>한상훈 장이화 정지원 ...</div>
-      </div>
-    </Container>
+        <div>한상훈 장이화 정지원 이승태 최영은 김의왕 h622 ...</div>
+      </EndingCredit>
+    </FooterContainer>
   );
 }
 
